213aaa6893ffb6025b17ca37b88d3d51253c0bd5
Pablo <p.gonzalez7027@gmail.com>
2019-09-05 14:52:01 -0500

    py file added

---
+++ calculadora.py
@@ -0,0 +1,66 @@
+import os
+import sys
+import time
+import datetime
+import asyncio
+import logging  #.log
+
+#in module
+class cal():
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def add(self):
+        return self.a+self.b
+    def mul(self):
+        return self.a*self.b
+    def div(self):
+        return self.a/self.b
+    def sub(self):
+        return self.a-self.b
+    
+
+def run():
+    a=int(input("Enter first number: "))
+    b=int(input("Enter second number: "))
+    obj=cal(a,b)
+    choice=1
+    while choice!=0:
+        print("0. Exit")
+        print("1. Add")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        choice=int(input("Enter choice: "))
+        if choice==1:
+            print("Result: ",obj.add())
+        elif choice==2:
+            print("Result: ",obj.sub())
+        elif choice==3:
+            print("Result: ",obj.mul())
+        elif choice==4:
+            print("Result: ",round(obj.div(),2))
+        elif choice==0:
+            print("Exiting!")
+        else:
+            print("Invalid choice!!")
+    print()
+
+def log():
+    logging.basicConfig(
+        #filename='logger.log',filemode='a', 
+        format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
+        level=logging.DEBUG)
+
+#main functions  
+def main():
+    log()
+    run()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
